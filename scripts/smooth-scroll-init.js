1412a4526d53cb8717ec1534d8727da29ae2ffca
idiotonuni <danielnavetta@gmail.com>
2015-11-10 20:16:58 -0800

    initial commit

---
+++ scripts/smooth-scroll-init.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    smoothScroll.init({
+        offset: 20,
+        speed: 1000,
+        easing: 'easeInOutQuint', // Easing pattern to use
+        updateURL: false, //hides the #scrollLinks
+    });
+})
