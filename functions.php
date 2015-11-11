068adec002567097610fbf2414300f8cebf05d7e
idiotonuni <danielnavetta@gmail.com>
2015-11-11 11:17:57 -0800

    Added addTextToFile function

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php
+    /**This function takes in a string that is the name of the text file stored on the server.
+     *It opens that text file for reading, then extracts all of the lines and builds a table out of it
+     *Once the table has been built and put into a string var, it returns that string to the calling method
+     */
     function getCacheData($textFile){
         $cache = fopen($textFile, "r");
         if($cache){
             //start building the output with an opening table element
             $output = "<table>\n<tbody>\n";
             //while not at the end of the file
+            $loopCounter = 0;
             while(!feof($cache)) {
                 //set $line to the next line in the file
                 $line = fgets($cache);
@@ -17,20 +22,49 @@
                     $output .= "<tr>\n";
                     //add all the td elements
                     foreach($line as $element){
-                        $output .= "<td>$element</td>\n";
+                        //clear extra spaces around element
+                        $element = trim($element);
+                        //if this is the fist loop then make the elements table headers
+                        if($loopCounter==0){
+                            $output .= "<th>$element</th>\n";
+                        }
+                        else{
+                            $output .= "<td>$element</td>\n";
+                        }
                     }
                     //$close off the table row element </tr>
                     $output .= "</tr>\n";
-                }
-                
+                    //increment loop counter
+                    $loopCounter .= 1;
+                }                
             }
             //close of the table element </table>
             $output .= "</tbody>\n</table>\n";
-            //close the text file
-            fclose($cache);
-        }else{
+        }else{ //if the file cannot be opened then return false
             $output = false;
         }
+        //close the text file
+        fclose($cache);
         return $output;
+    }
+    
+    //Self note, change permisions of local file to www-data ownership for this to work
+    //sudo chown -R www-data:www-data [$textFile]
+    function addTextToFile($textFile,$name,$date,$location,$comments){
+        if(is_writable($textFile)){
+            $cache = fopen($textFile,"a");
+            //use str_replace to get rid of all existing '|' symbols
+            $name = str_replace("|","-",$name);
+            $date = str_replace("|","-",$date);
+            $location = str_replace("|","-",$location);
+            $comments = str_replace("|","-",$comments);
+            //build the line to insert into the text file
+            $output = $name."|".$date."|".$location."|".$comments."\n";
+            fwrite($cache, $output);
+            fclose($cache);
+        }else{
+            echo "File Not Writable";
+        }
+        
     }
 ?>
