49e84941a8ef6556c2c95872faa7f61e33fd9051
idiotonuni <danielnavetta@gmail.com>
2015-11-18 21:02:30 -0800

    small fixes

--- scripts/getLocation.js
+++ scripts/getLocation.js
@@ -7,8 +7,6 @@ $(document).ready(function() {
             var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&sensor=true";
             var address = $.ajax(url)
             .done(function(data){
-               alert("success");
-               alert(data.results[1].formatted_address);
                fillLocation(data.results[1].formatted_address);
             })
             .fail(function(){
