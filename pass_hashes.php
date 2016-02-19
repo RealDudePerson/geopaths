c4cb6f924739502750269d3b1a1acb5e2f998f58
Daniel Navetta <Daniel Navetta>
2016-02-18 17:28:20 -0800

    -Changed color, added more hashes, no more foundation css

--- pass_hashes.php
+++ pass_hashes.php
@@ -6,6 +6,12 @@ function getHashes(){
            'Token2' => '$2y$10$MhL5KLKWAH9wLw.fpk5mC.bB.4FW/uzkIe7vg4S5UXVObWvHlW1Zq',
            'Token3' => '$2y$10$69CwRl/LOh0F6wug/Wd2f.T8xNBaYfY9RJBCxhQPGv9wUfH4qTdPi',
            'Token4' => '$2y$10$uZAAsu1Q21YDBFDA4.kiD.JeWwJADlzWCO7GAuN4j8WImdPI9L8bS',
+           'Token5' => '$2y$10$LENvN7b4JTFodZG34bwot.ELB7UdxHv7nbo.kPMLOhFI/gVndzf/K',
+           'Token6' => '$2y$10$ztaQd1cntOQ5cBpMGWfrIev1ens7q/7JhIT3Zqo1oJg1R0gkmVQLW',
+           'Token7' => '$2y$10$BMlJGimkFnRd1Ad5D/zG.eKIsXyrUKkLNr39moVkSPp1pLkwfutyG',
+           'Token8' => '$2y$10$ESXvm85Hicqj2wY9WycAAuPE1SfNVWezGfpfP6bNhuKOI.ftqYr0q',
+           'Token9' => '$2y$10$JdkDVo1fDS3utCV/BWwGreKE9pGzjRa0H1jx1oiB5OIJACcCT/59m',
+           'Token10' => '$2y$10$GN2tZfIvw5Nep1BKK3.yz.PHxB6OGDxjGADyh0gOwo1cfLfPFBzUG',
            );
     return $hashes;
 }
